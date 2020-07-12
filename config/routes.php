5eb8ff09f7bb897a14cd988b8a34be1bd003c38c
Chris Rowles <me@rowles.ch>
2020-07-12 13:04:00 +0100

    Create notifications

--- config/routes.php
+++ config/routes.php
@@ -19,19 +19,19 @@ $app['router']->with('/blog', function () use ($app, $blog) {
     $app['router']->get('', function () use ($blog) {
         return $blog->home(['title' => 'Blog']);
     });
-    $app['router']->get('/create', function () use ($blog) {
-        return $blog->create(['title' => 'New Post']);
-    });
+//    $app['router']->get('/create', function () use ($blog) {
+//        return $blog->create(['title' => 'New Post']);
+//    });
     $app['router']->get('/[i:id]/view', function ($request) use ($blog) {
         return $blog->view($request->id);
     });
-    $app['router']->get('/[i:id]/edit', function ($request) use ($blog) {
-        return $blog->edit($request->id);
-    });
-    $app['router']->post('/[i:id]?/submit', function ($request, $response) use ($blog) {
-        return $blog->submit($request, $response);
-    });
-    $app['router']->get('/[i:id]/delete', function ($request, $response) use ($blog) {
-        return $blog->delete($request, $response);
-    });
+//    $app['router']->get('/[i:id]/edit', function ($request) use ($blog) {
+//        return $blog->edit($request->id);
+//    });
+//    $app['router']->post('/[i:id]?/submit', function ($request, $response) use ($blog) {
+//        return $blog->submit($request, $response);
+//    });
+//    $app['router']->get('/[i:id]/delete', function ($request, $response) use ($blog) {
+//        return $blog->delete($request, $response);
+//    });
 });
