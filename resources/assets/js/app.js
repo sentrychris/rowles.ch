abce03fa73108ae797e6c0dd72802cc2d79bc944
Chris Rowles <me@rowles.ch>
2020-07-12 13:15:29 +0100

    Various changes

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -3,10 +3,10 @@ import Swal from 'sweetalert2';
 window.swal = Swal;
 
 import { library, dom } from '@fortawesome/fontawesome-svg-core';
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope, faHeart } from '@fortawesome/free-solid-svg-icons';
 import { faFacebookF, faGithub, faTwitter } from '@fortawesome/free-brands-svg-icons';
 
-library.add(faEnvelope, faFacebookF, faGithub, faTwitter );
+library.add(faEnvelope, faHeart, faFacebookF, faGithub, faTwitter );
 dom.watch();
 
 import { notification } from './notification';
