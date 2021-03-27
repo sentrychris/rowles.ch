d3985a1929cb1aa5bc9baa6a8e8797cc4576dd0a
Chris <me@rowles.ch>
2021-03-27 09:38:10 +0000

    all

--- config/routes.php
+++ config/routes.php
@@ -4,34 +4,41 @@
  | Configure application routes           |
  ----------------------------------------*/
 
-/** @var \Rowles\Controllers\PageController $page */
+use Pimple\Container;
+use Rowles\Controllers\BlogController;
+use Rowles\Controllers\PageController;
+
+/**
+ * @var Container $app
+ * @var PageController $page
+ * @var BlogController $blog
+ */
+
 $app['router']->get('/', function () use ($page) {
     return $page->home();
 });
 
-/** @var \Rowles\Controllers\PageController $page */
 $app['router']->get('/experience', function () use ($page) {
     return $page->experience(['title' => 'Experience']);
 });
 
-/** @var \Rowles\Controllers\BlogController $blog */
 $app['router']->with('/blog', function () use ($app, $blog) {
     $app['router']->get('', function () use ($blog) {
         return $blog->home(['title' => 'Blog']);
     });
-//    $app['router']->get('/create', function () use ($blog) {
-//        return $blog->create(['title' => 'New Post']);
-//    });
+    $app['router']->get('/create', function () use ($blog) {
+        return $blog->create(['title' => 'New Post']);
+    });
     $app['router']->get('/[i:id]/view', function ($request) use ($blog) {
         return $blog->view($request->id);
     });
-//    $app['router']->get('/[i:id]/edit', function ($request) use ($blog) {
-//        return $blog->edit($request->id);
-//    });
-//    $app['router']->post('/[i:id]?/submit', function ($request, $response) use ($blog) {
-//        return $blog->submit($request, $response);
-//    });
-//    $app['router']->get('/[i:id]/delete', function ($request, $response) use ($blog) {
-//        return $blog->delete($request, $response);
-//    });
+    $app['router']->get('/[i:id]/edit', function ($request) use ($blog) {
+        return $blog->edit($request->id);
+    });
+    $app['router']->post('/[i:id]?/submit', function ($request, $response) use ($blog) {
+        return $blog->submit($request, $response);
+    });
+    $app['router']->get('/[i:id]/delete', function ($request, $response) use ($blog) {
+        return $blog->delete($request, $response);
+    });
 });
