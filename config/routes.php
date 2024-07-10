86ddb393290631ca79e29340bfeb718397f3fe62
Chris Rowles <c.rowles@sentrybay.com>
2024-07-10 03:18:44 +0100

    cool

--- config/routes.php
+++ config/routes.php
@@ -3,11 +3,5 @@
 /*----------------------------------------
  | Configure application routes           |
  ----------------------------------------*/
-/**
- * Home page route.
- *
- * @var HomeController $home
- */
-$app['router']->get('/', function() use ($app) {
-    return $app[Rowles\Controllers\HomeController::class]->index();
-});
+
+$app['router']->get('/', fn() => $app[Rowles\Controllers\HomeController::class]->index());
