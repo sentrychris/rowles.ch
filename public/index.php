d17fe1a11a921f6f11dcee2958dc47c1c4be3390
Chris Rowles <c.rowles@sentrybay.com>
2024-07-10 17:54:27 +0100

    migrate to versyx

--- public/index.php
+++ public/index.php
@@ -1,11 +1,6 @@
 <?php
 
-use FastRoute\Dispatcher;
-use Psr\Http\Message\ResponseInterface;
-use Laminas\Diactoros\Response\HtmlResponse;
-use Laminas\Diactoros\ServerRequestFactory;
-use Laminas\HttpHandlerRunner\Emitter\SapiEmitter;
-use Versyx\View\ViewEngineInterface;
+use Versyx\Kernel;
 
 /*----------------------------------------
  | Bootstrap the application              |
@@ -15,45 +10,4 @@ require_once __DIR__.'/../config/bootstrap.php';
 /*----------------------------------------
  | Dispatch the request-response cycle    |
  ----------------------------------------*/
- $request = ServerRequestFactory::fromGlobals(
-    $_SERVER,
-    $_GET,
-    $_POST,
-    $_COOKIE,
-    $_FILES
-);
-
-$route = $app['router']->dispatch(
-    $request->getMethod(),
-    $request->getUri()->getPath()
-);
-
-switch ($route[0]) {
-    case Dispatcher::NOT_FOUND:
-        $response = new HtmlResponse(
-            $app[ViewEngineInterface::class]->render('error/404.twig')
-        );
-        break;
-    case Dispatcher::METHOD_NOT_ALLOWED:
-        $response = new HtmlResponse(
-            $app[ViewEngineInterface::class]->render('error/500.twig')
-        );
-        break;
-    case Dispatcher::FOUND:
-        $handler = $route[1];
-        $routeParams = $route[2];
-        [$class, $method] = $handler;
-        
-        $controller = $app[$class];
-        $response = $controller->$method($request, $routeParams);
-
-        if (! $response instanceof ResponseInterface) {
-            throw new \RuntimeException(
-                $class. '->'.$method.' must return a valid PSR-7 response'
-            );
-        }
-
-        break;
-}
-
-(new SapiEmitter())->emit($response);
+Kernel::dispatch($app);
