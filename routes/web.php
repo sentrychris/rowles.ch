3f7e0c72fc8a6a424ec9c95947f34a2b8d02e81b
Chris Rowles <c.rowles@sentrybay.com>
2024-07-14 23:50:35 +0100

    all

--- routes/web.php
+++ routes/web.php
@@ -5,22 +5,28 @@
  ----------------------------------------*/
  
 return [
-    ['GET', '/', [App\Http\Controllers\HomeController::class, 'index']],
+    '/' => [
+        ['GET', '/', [App\Http\Controllers\HomeController::class, 'index']]
+    ],
 
-    // Simple CMS
-    ['GET', '/cms', [App\Http\Controllers\CMS\HomeController::class, 'index']],
+    '/cms' => [
 
-    // Employment history management
-    ['GET',  '/cms/employment-history/create',    [App\Http\Controllers\CMS\EmploymentHistoryController::class, 'create']],
-    ['POST', '/cms/employment-history/create',    [App\Http\Controllers\CMS\EmploymentHistoryController::class, 'store']],
-    ['GET',  '/cms/employment-history/edit/{id}', [App\Http\Controllers\CMS\EmploymentHistoryController::class, 'edit']],
-    ['PUT',  '/cms/employment-history/edit/{id}', [App\Http\Controllers\CMS\EmploymentHistoryController::class, 'update']],
-    ['DELETE', '/cms/employment-history/delete/{id}', [App\Http\Controllers\CMS\EmploymentHistoryController::class, 'delete']],
+        ['GET', '/', [App\Http\Controllers\CMS\HomeController::class, 'index']],
 
-    // Contact detail management
-    ['GET',  '/cms/contact-detail/create',    [App\Http\Controllers\CMS\ContactDetailController::class, 'create']],
-    ['POST', '/cms/contact-detail/create',    [App\Http\Controllers\CMS\ContactDetailController::class, 'store']],
-    ['GET',  '/cms/contact-detail/edit/{id}', [App\Http\Controllers\CMS\ContactDetailController::class, 'edit']],
-    ['PUT',  '/cms/contact-detail/edit/{id}', [App\Http\Controllers\CMS\ContactDetailController::class, 'update']],
-    ['DELETE', '/cms/contact-detail/delete/{id}', [App\Http\Controllers\CMS\ContactDetailController::class, 'delete']]
+        'employment-history' => [
+            ['GET',    '/create',      [App\Http\Controllers\CMS\EmploymentHistoryController::class, 'create']],
+            ['POST',   '/create',      [App\Http\Controllers\CMS\EmploymentHistoryController::class, 'store']],
+            ['GET',    '/edit/{id}',   [App\Http\Controllers\CMS\EmploymentHistoryController::class, 'edit']],
+            ['PUT',    '/edit/{id}',   [App\Http\Controllers\CMS\EmploymentHistoryController::class, 'update']],
+            ['DELETE', '/delete/{id}', [App\Http\Controllers\CMS\EmploymentHistoryController::class, 'delete']],
+        ],
+
+        'contact-detail' => [
+            ['GET',    '/create',      [App\Http\Controllers\CMS\ContactDetailController::class, 'create']],
+            ['POST',   '/create',      [App\Http\Controllers\CMS\ContactDetailController::class, 'store']],
+            ['GET',    '/edit/{id}',   [App\Http\Controllers\CMS\ContactDetailController::class, 'edit']],
+            ['PUT',    '/edit/{id}',   [App\Http\Controllers\CMS\ContactDetailController::class, 'update']],
+            ['DELETE', '/delete/{id}', [App\Http\Controllers\CMS\ContactDetailController::class, 'delete']],
+        ],
+    ],
 ];
